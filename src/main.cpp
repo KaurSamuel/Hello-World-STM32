f18318b07f79d1b71958d16f884fffad32b6a844
Tudeng Tudeng <tudeng@tktk.ee>
2021-02-10 10:57:06 +0200

    Switch led using PB12

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include <Arduino.h>
 
+int val = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PB12,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(200);
+
+  //val = digitalRead(PB_12);
+  if (digitalRead(PB12))
+  {
+      digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+      digitalWrite(LED_BUILTIN, LOW);
+  }
+  
+/*  delay(200);
   digitalWrite(LED_BUILTIN, LOW);
   delay(200);
   digitalWrite(LED_BUILTIN, HIGH);
+*/
 }
